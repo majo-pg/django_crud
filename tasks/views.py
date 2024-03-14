629e594c1df103894b7c1e36512027af46b51803
MJPG <ada_hack530@womenintech.mx>
2024-03-13 18:19:22 -0600

    first commit

---
+++ tasks/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+from .forms import TaskForm
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {'form': UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    request.POST['username'], password=request.POST
+                    ['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render (request, 'signup.html', {
+                    'form': UserCreationForm, 
+                    'error':'User already exists'
+                 })
+               
+        return render (request, 'signup.html', {
+                    'form': UserCreationForm, 
+                    'error':'Password do not match'
+        })
+    
+@login_required
+def task(request):
+    return render(request, 'tasks.html')
+
+@login_required
+def create_task(request):
+    if request.method =='GET':
+        return render(request, 'create_task.html', {'form': TaskForm})
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            print(new_task)
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {'form': TaskForm, 'error': 'Please provide valid data'})
+
+@login_required
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {'form': AuthenticationForm })
+    
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+           return render(request, 'signin.html', {'form': AuthenticationForm, 'error': 'Username or password is incorrect'})
+        else:
+             login(request, user)
+             return redirect('task') 
